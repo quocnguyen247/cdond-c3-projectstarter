c1773f23ab5982fb9007228acd776d3b60358457
atlantis333 <azcert20infra.cip.vip@gmail.com>
2023-07-29 10:54:26 +0700

    test

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=xuqupro/udacity
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+echo dckr_pat_ZCfGix_GkZ_TicEv0277oyYMHDc | docker login -u xuqupro
+
+docker tag appml:v1.0.1 $dockerpath/appml:v1.0.1
 
 # Step 3:
 # Push image to a docker repository
+docker push  $dockerpath/appml:v1.0.1
